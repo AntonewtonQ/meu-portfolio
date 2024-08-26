efeca15ff4e3c99f9761ffda777cb925773ae7d9
AntonewtonQ <antonewtonquima@gmail.com>
2024-08-26 22:18:17 +0100

    tá chill 003

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -4,6 +4,17 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyDGMfwKIS_T87j9kAKBjmGgfFub9_p4MiQ",
+  authDomain: "antonewtonq.firebaseapp.com",
+  projectId: "antonewtonq",
+  storageBucket: "antonewtonq.appspot.com",
+  messagingSenderId: "664241737651",
+  appId: "1:664241737651:web:0340b3b9a7eda4315045c6",
+  measurementId: "G-FFJLM7VS16"
+};
+
+/*
+  const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -12,6 +23,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+*/
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -19,3 +31,5 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { auth,db };
+
+
